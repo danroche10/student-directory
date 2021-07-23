fdeb18bc6ff92c81b6da56c7385c706ba8475748
danroche10 <danielroche10@gmail.com>
2021-07-23 09:07:01 +0100

    ex8 completed

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,6 +1,7 @@
-def input_students
+months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
 
-  months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+def input_students(months)
 
   puts "Please enter some student details"
   puts "To finish, just hit return twice"
@@ -9,40 +10,26 @@ def input_students
   # get the first name
   puts "Name:"
   name = gets.chomp
-  if name.length > 0
+  
+  # while the name is not empty, repeat this code
+  while !name.empty? do
     puts "Hobbies:"
     hobbies = gets.chomp
     puts "Country of birth:"
     birthplace = gets.chomp
     birthplace == "" ? birthplace = "unknown" : birthplace = birthplace
     puts "Cohort"
-    cohort = gets.chomp
-    while !months.include?(cohort.downcase) do
+    cohort = gets.chomp.downcase
+    while !months.include?(cohort) do
       puts "This cohort isn't recognised. Please try again."
-      cohort = gets.chomp
+      cohort = gets.chomp.downcase
     end
-  end
- 
-  # while the name is not empty, repeat this code
-  while !name.empty? do
     # add the student hash to the array
     students << {name: name, hobbies: hobbies, birthplace: birthplace, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
     # gets another name from the user
     puts "Name:"
     name = gets.chomp
-    if name.length > 0
-      puts "Hobbies:"
-      hobbies = gets.chomp
-      puts "Country of birth:"
-      birthplace = gets.chomp
-      birthplace == "" ? birthplace = "unknown" : birthplace = birthplace
-      puts "Cohort"
-    while !months.include?(cohort.downcase) do
-      puts "This cohort isn't recognised. Please try again."
-      cohort = gets.chomp
-    end
-    end
   end
   # return the array of students
   students
@@ -54,17 +41,23 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-      puts ("#{index+1}. #{student[:name]} Hobbies: #{student[:hobbies]}, Birthplace: #{student[:birthplace]}, (#{student[:cohort]} cohort)").center(100, "-----")
-  end
+    cohorts = students.map { |student| student[:cohort] }.uniq
+    cohorts.each do |cohort|
+      puts cohort.capitalize()
+      students.each_with_index do |student, index|
+        if cohort == student[:cohort]
+          puts ("#{index+1}. Name: #{student[:name]}, Hobbies: #{student[:hobbies]}, Birthplace: #{student[:birthplace]}, (#{student[:cohort]} cohort)").center(100, "-----")
+        end
+      end
+    end
 end
 
 def print_footer(students)
-    puts ("Overall, we have #{students.count} great students").center(100, "-----")
+    puts ("Overall, we have #{students.count} great #{students.count > 1 ? "students" : "student"}").center(100, "-----")
 end
 
 # nothing happens until we call the methods
-students = input_students
+students = input_students(months)
 print_header
 print(students)
 print_footer(students)
